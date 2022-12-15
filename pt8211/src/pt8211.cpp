f17c6be2e786b9c94954bc876a13f9ee797430df
rapsacw <66800349+rapsacw@users.noreply.github.com>
2022-12-15 14:25:47 +0100

    Add files via upload

--- pt8211/src/pt8211.cpp
+++ pt8211/src/pt8211.cpp
@@ -1,30 +1,36 @@
 #include <pt8211.h>
+#include <Arduino.h>
 
 // PT8211 audio R-2R dac
 // 2 16 bit outputs (left+right)
-// input: 32bit, upper 16bit for right output (wsl low), lower 16 bit for left channel (wsl high)
-// wsl,din sampled on rising edge of bck and data is output on wsl change and rising edge of bck
-// so the output of the left channel is set on the 2nd call to PT8211. If the left channel is used call PT811_out 2 times with the same data
+// input: 16bit short integer for left output, 16 bit short integer for right channel
+// wsl,din sampled on rising edge of bck and data is output after receiving 16 bits of data for each channel.
 // The output voltage can be set between ~0.8V .. ~2.5V in 65536 steps
 
 unsigned char pin_8211_din,pin_8211_bck,pin_8211_ws;
 
-void PT8211_out(unsigned int LRanalog)
+void PT8211_out(unsigned short left, unsigned short right)
 {
   int i;
+  unsigned int lr;
 
-
+  // bounds check/limiting
+  if(left<1) left = 1;
+  if(right<1) right = 1;
+  if(left>0xfffe) left = 0xfffe;
+  if(right>0xfffe) right = 0xfffe;
+  lr = (right<<16) | left;
   // convert to 'strange' 2-complement used by pt8211
-  LRanalog -= 0x80008000;
+  lr -= 0x80008000;
   for(i=0;i<32;i++)
   {
     if(i == 16)
       digitalWrite(pin_8211_ws, 1);
 
-    digitalWrite(pin_8211_din, (LRanalog&0x80000000)?1:0);
+    digitalWrite(pin_8211_din, (lr&0x80000000)?1:0);
     digitalWrite(pin_8211_bck, 1);
     digitalWrite(pin_8211_bck, 0);
-    LRanalog <<= 1;
+    lr <<= 1;
   }
   digitalWrite(pin_8211_ws, 0);
 }
@@ -40,6 +46,5 @@ void PT8211_init(char Dpin_din,char Dpin_bck,char Dpin_ws)
   digitalWrite(pin_8211_din, 0);
   digitalWrite(pin_8211_bck, 0);
   digitalWrite(pin_8211_ws, 0);
-  PT8211_out(0);
-  PT8211_out(0);
+  PT8211_out(0,0);
 }
